0478f6cf50b81b4da245f803441a264ddaa6d768
KomalAli05 <350323770@gapps.yrdsb.ca>
2022-05-03 13:31:56 -0400

    Made bumblebee move

---
+++ bumbleBee.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bumbleBee here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bumbleBee extends Actor
+{
+    /**
+     * Act - do whatever the bumbleBee wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
