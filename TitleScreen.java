d6183f2005cc99475574fe79765b75108253e8af
Stewart Chan <stew.chan@gmail.com>
2022-05-20 14:04:05 -0400

    Reorganized titlescreen labels

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titlePage = new Label("Welcome to the Hungry Bumblebee game!", 35);
+    
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,7 +18,7 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        addObject(titlePage, getWidth()/2, getHeight()/2);
+        
         prepare();
     }
 
@@ -44,20 +44,18 @@ public class TitleScreen extends World
     {
         BumbleBee bumbleBee = new BumbleBee();
         addObject(bumbleBee,306,317);
-        Label label = new Label("Hit enter to start!", 40);
-        addObject(label,293,187);
-        label.setLocation(376,156);
-        label.setLocation(394,159);
-        Label label2 = new Label("Use left and right arrow keys to play :)", 25);
-        addObject(label2,300,228);
-        label2.setLocation(324,230);
-        label.setLocation(299,183);
-        label2.setLocation(305,128);
-        bumbleBee.setLocation(367,281);
-        bumbleBee.setLocation(321,357);
-        label.setLocation(319,207);
-        bumbleBee.setLocation(316,309);
-        label2.setLocation(325,159);
+        
+        Label label = new Label("Welcome to the Hungry Bumblebee game!", 35);
+        addObject(label,291,41);
+        label.setLocation(302,51);
+        
+        Label label2 = new Label("Hit enter to start!", 40);
+        addObject(label2,294,165);
+        label2.setLocation(303,134);
+        
+        Label label3 = new Label("Use left and right arrow keys to play :)", 25);
+        addObject(label3,303,209);
+        label3.setLocation(309,206);
     }
 }
 
