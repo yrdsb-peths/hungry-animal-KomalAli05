ee07fce2431e11a31225fba44f2cdc8964d45ab3
KomalAli05 <350323770@gapps.yrdsb.ca>
2022-05-23 16:31:41 -0400

    Continuing to change pumpkin size

--- Pumpkin.java
+++ Pumpkin.java
@@ -9,7 +9,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Pumpkin extends Actor
 {
     int speed = 1;
-    Pumpkin pumpkin = new Pumpkin();
     public void act()
     {
         // Pumpkin falls 
@@ -24,7 +23,7 @@ public class Pumpkin extends Actor
         {
             world.gameOver();
             world.removeObject(this);
-            pumpkin.scale(pumpkin.getWidth() - 100, pumpkin.getHeight() - 100);
+            
         }
     }
     
